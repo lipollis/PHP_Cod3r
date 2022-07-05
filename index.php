a0de569bbbe9dcc59bb29762adef5b1331f3c8bb
Aline Pollis <aline.pollis@hotmail.com>
2022-07-05 11:51:06 -0300

    Fim do capítulo 4 - tipos

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
@@ -28,6 +28,21 @@
                         <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
                     </ul>
                 </div>
+
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Inteiros</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=float">Float </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Aritméticas </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=string">String </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=booleano">Booleano </a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões </a></li>
+                    </ul>
+                </div>
+
 
             </nav>
         </div>
---
+++ tipos/int.php
@@ -0,0 +1,17 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11); //tipo de variável
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+
+echo 011, '<br>';
+echo 017, '<br>'; //octal
+echo 0b11, '<br>'; //binario
+echo 0x11, '<br>'; //hexa
+

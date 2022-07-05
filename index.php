471f3ec6baa5807af0f504b66ede30de3ed59e88
Aline Pollis <aline.pollis@hotmail.com>
2022-07-05 16:56:11 -0300

    Fim do capítulo 6 - estrutura de controle

--- index.php
+++ index.php
@@ -57,6 +57,20 @@
                     </ul>
                 </div>
 
+                <div class="modulo roxo">
+                    <h3>Controle</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=controle&file=if_else">If Else</a></li>
+                        <li><a href="exercicio.php?dir=controle&file=operadores_relacionais">Op. Relacionais </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio Op. Lógicos </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador Ternário </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=switch">Switch </a></li>
+                        <li><a href="exercicio.php?dir=controle&file=desafio_switch">Desafio Switch </a></li>
+                    </ul>
+                </div>
+
             </nav>
         </div>
     </main>

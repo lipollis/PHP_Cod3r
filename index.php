3ff6601bee19e90b34d9dd14209a4a6fd1a07758
Aline Pollis <aline.pollis@hotmail.com>
2022-07-06 17:02:13 -0300

    Fim do capítulo 10 - Programação Orientada a Objeto com PHP

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
                 </div>
 
                 <div class="modulo verde-escuro">
-                    <h3>6. Repetições</h3>
+                    <h3>Repetições</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=repeticoes&file=for"> Laço For </a></li>
                         <li><a href="exercicio.php?dir=repeticoes&file=desafio_for"> Desafio For </a></li>
@@ -103,7 +103,7 @@
                 </div>
 
                 <div class="modulo vermelho-escuro">
-                    <h3>7. Funções</h3>
+                    <h3>Funções</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=funcoes&file=escopo"> Função & Escopo </a></li>
                         <li><a href="exercicio.php?dir=funcoes&file=args_retorno"> Args & Retorno </a></li>
@@ -119,6 +119,29 @@
                         <li><a href="exercicio.php?dir=funcoes&file=map_filter">  & Filter </a></li>
                     </ul>
                 </div>
+
+                <div class="modulo azul-escuro">
+                    <h3>Paradigma O.O.</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=classe"> Primeira Classe </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=data"> Classe Data </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=construtor_destrutor"> Const. e Destrutor </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=heranca"> Herança </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=visibilidade"> Visibilidade </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=static"> Membros Estáticos </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=interface"> Interface </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=abstract"> Classe Abstrata </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros"> Desafio Erros </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=final"> Modificador Final </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=traits_01"> Traits #01 </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=traits_02"> Traits #02 </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=magic_methods"> Métodos Mágicos </a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=polimorfismo"> Polimorfismo </a></li>
+                    </ul>
+                </div>
+
+
+
 
             </nav>
         </div>

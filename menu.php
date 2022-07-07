2948a08ad74a788ef2d10895febcd9953c3894ee
Aline Pollis <aline.pollis@hotmail.com>
2022-07-07 14:42:33 -0300

    Fim do capítulo 15 - api

--- menu.php
+++ menu.php
@@ -160,4 +160,17 @@
                     </ul>
                 </div>
 
+                <div class="modulo azul">
+                    <h3>1API</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=api&file=datas_01"> Datas #01 </a></li>
+                        <li><a href="exercicio.php?dir=api&file=datas_02"> Datas #02 </a></li>
+                        <li><a href="exercicio.php?dir=api&file=escrever_arquivo"> Escrever Arquivo </a></li>
+                        <li><a href="exercicio.php?dir=api&file=ler_arquivo"> Ler Arquivo </a></li>
+                        <li><a href="exercicio.php?dir=api&file=upload"> Upload </a></li>
+                        <li><a href="exercicio.php?dir=api&file=download"> Download </a></li>
+                        <li><a href="exercicio.php?dir=api&file=imagens"> Imagens </a></li>
+                    </ul>
+                </div>
+
             </nav>
